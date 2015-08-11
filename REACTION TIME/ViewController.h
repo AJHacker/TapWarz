1048cb436c5ee7aec9cb50654246f5a58494465c
AJHacker <arihant@quixoa.com>
2015-08-11 11:58:52 -0400

    ReactionTimeGame

--- REACTION TIME/ViewController.h
+++ REACTION TIME/ViewController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController{
+   IBOutlet UIButton *p1;
+   IBOutlet UIButton *p2;
+    IBOutlet UIButton *start;
+    IBOutlet UILabel *p2score;
+    IBOutlet UILabel *p1score;
+    IBOutlet UILabel *p1label;
+    IBOutlet UILabel *p2label;
+    double p1int;
+    int star;
+    int gg;
+    double p2int;
+}
+-(IBAction)start:(id)sender;
+-(IBAction)p1tap:(id)sender;
+-(IBAction)p2tap:(id)sender;
 @end
--- REACTION TIME/ViewController.m
+++ REACTION TIME/ViewController.m
@@ -14,9 +14,64 @@
 
 @implementation ViewController
 
+- (void)start:(id)sender{
+    [start setHidden:TRUE];
+    star = 1;
+    p1int=0;
+    p2int=0;
+    [self update];
+}
+- (void)p1tap:(id)sender{
+    if (star==1) {
+        p1int = p1int+1;
+        p2int = p2int-.5;
+        [self update];
+        
+    }
+}
+- (void)p2tap:(id)sender{
+    if (star==1) {
+        p1int = p1int-.5;
+        p2int = p2int+1;
+        [self update];
+    }
+    
+}
+
+-(void)update{
+    NSString *theValue = [NSString stringWithFormat:@"%g", p1int];
+    p1score.text=theValue;
+    NSString *theValue2 = [NSString stringWithFormat:@"%g", p2int];
+    p2score.text=theValue2;
+    if (p2int>=100) {
+        star=0;
+        [start setHidden:false];
+        p1label.text = @"YOU LOSE";
+        p2label.text = @"YOU WIN!";
+    }
+    
+    
+    
+    
+    if (p1int>=100) {
+        star=0;
+        [start setHidden:false];
+        p2label.text = @"YOU LOSE";
+        p1label.text = @"YOU WIN!";
+
+    }
+
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    p2score.transform = CGAffineTransformMakeRotation( ( 180 * M_PI ) / 180 );
+    p2label.transform = CGAffineTransformMakeRotation( ( 180 * M_PI ) / 180 );
+    [p2label setHidden:TRUE];
+    [p1label setHidden:TRUE];
+    star = 0;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
